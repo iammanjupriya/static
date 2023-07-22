1a9e7547d3f6a9511e9fe56be9b2d3dc76b62ec7
manju priya <106304226+iammanjupriya@users.noreply.github.com>
2023-07-22 14:25:38 +0530

    Add files via upload

---
+++ static/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @static
+{
+    internal class Program
+    {
+        class firstProgram
+        {
+            static int a;
+            int b;
+            static void Square()
+
+            {
+                Console.WriteLine("enter no");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("print " + a);
+            }
+            public void Square1()
+
+            {
+                Console.WriteLine("enter no");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("print " + b);
+            }
+            static void Main(string[] args)
+            {
+               firstProgram p = new firstProgram();
+                //int num1 = 5;
+                //int num2 = 10;
+                //  Console.WriteLine(num1 + " " + num2);
+                Square();
+                p.Square1();
+                // Console.WriteLine(num1 + " " + num2);
+                Console.WriteLine("Press Enter Key to Exit..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+    
+
